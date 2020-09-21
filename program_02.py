972a4791771b37f65db65ec633bf64e02452dd8a
pratikrohane <pratikhrohane124@gmail.com>
2020-09-22 04:04:36 +0530

    Simple Interest

---
+++ program_02.py
@@ -0,0 +1,10 @@
+#Find simple interest(SI) for the given principal amount(P), 
+#time(T) and rate of interest(R)
+
+P = int(input())
+R = int(input())
+T = int(input())
+
+SI = (P * R * T)/ 100
+
+print("Simple Interest :",SI)
