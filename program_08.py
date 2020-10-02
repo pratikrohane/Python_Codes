add1135551414fdb1125d730d1b1637a49ba89d3
pratikrohane <pratikhrohane124@gmail.com>
2020-10-02 19:22:02 +0530

    Prime Number in an Interval

---
+++ program_08.py
@@ -0,0 +1,10 @@
+start = 12
+end = 66
+
+for i in range(start, end+1):
+    if i > 1:
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            print(i)
